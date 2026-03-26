042f7dcf07a10f0f54d3d58cce769b6b38e711c7
Sebastián Rupani <sebastian.rupani@factored.ai>
2026-03-26 16:40:36 +0000

    converted payment_amount form cents to dollars

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@ SELECT
     orderid AS order_id,
     INITCAP(REPLACE(paymentmethod, '_', ' ')) AS payment_method,
     INITCAP(status) AS payment_status,
-    amount AS payment_amount,
+    amount / 100 AS payment_amount,
     created AS payment_date
 
 FROM {{ source('stripe', 'payments') }}
